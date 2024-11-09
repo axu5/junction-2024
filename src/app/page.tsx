524912354e7e15dc3f5bd65567e7d25bf06d86e2
axu5 <axuissocool@gmail.com>
2024-11-10 00:31:55 +0200

    added sticky get started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Benefit } from "./benefits";
 export default function Home() {
   return (
     <>
-      <section className="h-[100vh]">
+      <section className="">
         <div className="flex flex-col xl:flex-row">
           <div className="flex flex-col">
             <div className="flex flex-col gap-y-5 py-20 xl:max-w-[60%]">
@@ -21,12 +21,6 @@ export default function Home() {
               </h2>
               <p>Made with ❤️ in Junction</p>
             </div>
-            {/* <Link
-          className="bg-primary flex flex-row justify-center rounded-full p-3 text-center text-foreground"
-          href="/job-quiz"
-        >
-          Get Started <ArrowRight />
-        </Link> */}
           </div>
           <div className="flex flex-col justify-center gap-y-3 xl:w-[40%]">
             <Benefit
@@ -43,25 +37,25 @@ export default function Home() {
             />
           </div>
         </div>
-        <Link
-          href="/job-quiz"
-          className="font-staatliches absolute bottom-20 left-1/2 mx-auto my-5 flex -translate-x-1/2 -translate-y-1/2 flex-row justify-center rounded-full bg-highlight px-16 py-5 text-center text-3xl text-foreground"
-        >
-          Get started
-        </Link>
-        <Image
-          src={HustleHiveArrowDown}
-          alt="Scroll down for more"
-          className="motion-preset-oscillate absolute bottom-10 right-20 w-16 object-cover object-center"
-        />
       </section>
+      <Link
+        href="/job-quiz"
+        className="sticky top-10 mx-auto flex max-w-2xl flex-row justify-center rounded-full bg-highlight py-5 text-center font-staatliches text-3xl text-foreground"
+      >
+        Get started
+      </Link>
+      <Image
+        src={HustleHiveArrowDown}
+        alt="Scroll down for more"
+        className="motion-preset-oscillate absolute bottom-10 right-20 w-16 object-cover object-center"
+      />
 
       <section className="flex h-[75vh] flex-col items-center justify-center gap-y-10">
-        <h2 className="bg-secondary font-staatliches rounded-xl p-12 text-2xl text-foreground">
+        <h2 className="rounded-xl bg-secondary p-12 font-staatliches text-2xl text-foreground">
           Aligning your career with your values has a direct impact on your
           overall satisfaction, motivation, and well-being.
         </h2>
-        <h2 className="bg-secondary font-staatliches rounded-xl p-12 text-2xl text-foreground">
+        <h2 className="rounded-xl bg-secondary p-12 font-staatliches text-2xl text-foreground">
           Roles that respect work-life balance support mental and physical
           health, preventing burnout and helping you stay present in all areas
           of life.
