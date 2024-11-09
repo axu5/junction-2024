a66000c2472d405f287e89a508289473fc1a6fbb
Joni Savolainen <joni@savolainen.io>
2024-11-10 00:24:04 +0200

    Fix skill issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import HustleHiveArrowDown from "@/../public/arrow-down.png";
 import Image from "next/image";
 import Link from "next/link";
 import { Benefit } from "./benefits";
-import { HexagonSharp } from "./hexagon";
 
 export default function Home() {
   return (
@@ -46,7 +45,7 @@ export default function Home() {
         </div>
         <Link
           href="/job-quiz"
-          className="absolute bottom-20 left-1/2 mx-auto my-5 flex -translate-x-1/2 -translate-y-1/2 flex-row justify-center rounded-full bg-highlight px-16 py-5 text-center font-staatliches text-3xl text-foreground"
+          className="font-staatliches absolute bottom-20 left-1/2 mx-auto my-5 flex -translate-x-1/2 -translate-y-1/2 flex-row justify-center rounded-full bg-highlight px-16 py-5 text-center text-3xl text-foreground"
         >
           Get started
         </Link>
@@ -58,11 +57,11 @@ export default function Home() {
       </section>
 
       <section className="flex h-[75vh] flex-col items-center justify-center gap-y-10">
-        <h2 className="rounded-xl bg-secondary p-12 font-staatliches text-2xl text-foreground">
+        <h2 className="bg-secondary font-staatliches rounded-xl p-12 text-2xl text-foreground">
           Aligning your career with your values has a direct impact on your
           overall satisfaction, motivation, and well-being.
         </h2>
-        <h2 className="rounded-xl bg-secondary p-12 font-staatliches text-2xl text-foreground">
+        <h2 className="bg-secondary font-staatliches rounded-xl p-12 text-2xl text-foreground">
           Roles that respect work-life balance support mental and physical
           health, preventing burnout and helping you stay present in all areas
           of life.
