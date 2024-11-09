886a2223ea021ae9a673be23144c0a88a0c18c17
Joni Savolainen <joni@savolainen.io>
2024-11-10 00:02:31 +0200

    Guard against potential json markdown AI shenanigans

--- src/app/api/chat-server.ts
+++ src/app/api/chat-server.ts
@@ -4,22 +4,22 @@ import { NextResponse } from "next/server";
 type ChatOptions = {
   system: string;
   prompt: string;
-}
+};
 
 export async function chat(options: ChatOptions) {
   const completion = await openai.chat.completions.create({
-    model: 'gpt-4o-mini',
+    model: "gpt-4o-mini",
     stream: true,
     messages: [
       {
         role: "system",
-        content: options.system
+        content: options.system,
       },
       {
         role: "user",
-        content: options.prompt
-      }
-    ]
+        content: options.prompt,
+      },
+    ],
   });
 
   return new NextResponse(completion.toReadableStream(), {
@@ -31,20 +31,26 @@ export async function chat(options: ChatOptions) {
 
 export async function json(options: ChatOptions) {
   const completion = await openai.chat.completions.create({
-    model: 'gpt-4o-mini',
+    model: "gpt-4o-mini",
     messages: [
       {
         role: "system",
-        content: options.system
+        content: options.system,
       },
       {
         role: "user",
-        content: options.prompt
-      }
-    ]
+        content: options.prompt,
+      },
+    ],
   });
 
-  return new NextResponse(completion.choices[0].message.content ?? "{}", {
+  const content = completion.choices[0].message.content ?? "{}";
+
+  const firstBrace = content.indexOf("");
+  const lastBrace = content.lastIndexOf("");
+  const filteredContent = content.slice(firstBrace, lastBrace + 1);
+
+  return new NextResponse(filteredContent, {
     headers: {
       "Content-Type": "application/json",
     },
