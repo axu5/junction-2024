2d675e85beccc6ec0dc037606e32e7dea115ba1e
Joni Savolainen <joni@savolainen.io>
2024-11-09 15:19:46 +0200

    Add chat helper

---
+++ src/app/api/chat.ts
@@ -0,0 +1,30 @@
+import openai from "@/app/ai";
+import { NextResponse } from "next/server";
+
+type ChatOptions = {
+  system: string;
+  prompt: string;
+}
+
+export async function chat(options: ChatOptions) {
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    stream: true,
+    messages: [
+      {
+        role: "system",
+        content: options.system
+      },
+      {
+        role: "user",
+        content: options.prompt
+      }
+    ]
+  });
+
+  return new NextResponse(completion.toReadableStream(), {
+    headers: {
+      "Content-Type": "text/event-stream",
+    },
+  });
+}
